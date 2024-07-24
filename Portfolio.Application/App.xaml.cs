2b076f537ea19e2612511240812fb83864474c21
Brandon Recce <brandon.recce@tek84.com>
2024-07-23 17:32:50 -0700

    Initial Project Commit

---
+++ Portfolio.Application/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Application
+{
+    public partial class App : Microsoft.Maui.Controls.Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Portfolio.Application/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Application
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
